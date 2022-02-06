99a1bff500d1ccf7d34a10e9b9e04a6a9dd27a27
Tim Kuhlmann <code@tinx.eu>
2022-02-06 15:08:20 +0100

    Add missing include

--- SSD1306_128x64_SPI.h
+++ SSD1306_128x64_SPI.h
@@ -5,6 +5,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <time.h>
 
 #define SCREEN_WIDTH 128 // OLED display width in pixels
 #define SCREEN_HEIGHT 64 // OLED display height in pixels
